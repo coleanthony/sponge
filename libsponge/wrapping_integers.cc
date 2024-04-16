7761e45c85fe6d5133879e7efdf2f6ba12a46e05
coleanthony <971259967@qq.com>
2024-04-16 21:38:44 +0800

    update wrapping_intergers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include<cmath>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -14,8 +15,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32{isn.raw_value()+static_cast<uint32_t>(n)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,14 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t diff = n.raw_value()-isn.raw_value();
+    uint64_t t = (checkpoint&0XFFFFFFFF00000000)+diff;
+    uint64_t res = t;
+    if(abs(int64_t(t+(1ul<<32)-checkpoint))<abs(int64_t(t-checkpoint))){
+        res=t+(1ul<<32);
+    }
+    if(t>=(1ul<<32)&&abs(int64_t(t-(1ul<<32)-checkpoint))<int64_t(res-checkpoint)){
+        res=t-(1ul<<32);
+    }
+    return res;
 }
