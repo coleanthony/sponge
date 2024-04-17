74ec16ff3a86b3f1e4526c4426c14069ba4c5bbc
coleanthony <971259967@qq.com>
2024-04-17 14:39:41 +0800

    finish lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -10,10 +10,36 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+/*
+!1.Listen waiting for syn:ackno is empty
+!2.syn_recv: ackno exists and input stream hasn't ended
+!3.fin_recv
+*/
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader &header = seg.header();
+    //listen阶段
+    if(!_is_sync_set){
+        if (!header.syn){
+            return;
+        }
+        _is_sync_set=true;
+        _isn=header.seqno;
+    }
+    //syn_recv阶段
+    uint64_t checkpoint=_reassembler.stream_out().bytes_written()+1;
+    uint64_t abs_no=unwrap(header.seqno,_isn,checkpoint);
+    uint64_t seqnum=abs_no-1+header.syn;
+    _reassembler.push_substring(seg.payload().copy(),seqnum,header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+std::optional<WrappingInt32> TCPReceiver::ackno() const {
+    if(!_is_sync_set){
+        return std::nullopt;
+    }
+    //第一个没有收到的，所以+1，fin标志位+1
+    return wrap(_reassembler.stream_out().bytes_written()+1+_reassembler.stream_out().input_ended(), _isn);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return _capacity-_reassembler.stream_out().buffer_size();
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -16,16 +16,17 @@
 class TCPReceiver {
     //! Our data structure for re-assembling bytes.
     StreamReassembler _reassembler;
-
     //! The maximum number of bytes we'll store.
     size_t _capacity;
+    bool _is_sync_set=false;
+    WrappingInt32 _isn;
 
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity),_isn(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
@@ -35,6 +36,7 @@ class TCPReceiver {
     //!
     //! This is the beginning of the receiver's window, or in other words, the sequence number
     //! of the first byte in the stream that the receiver hasn't received.
+    // 返回接收方尚未获取到的第一个字节的字节索引。如果 ISN 暂未被设置，则返回空。
     std::optional<WrappingInt32> ackno() const;
 
     //! \brief The window size that should be sent to the peer
@@ -47,6 +49,7 @@ class TCPReceiver {
     //! the first byte that falls after the window (and will not be
     //! accepted by the receiver) and (b) the sequence number of the
     //! beginning of the window (the ackno).
+    //返回接收窗口的大小，即第一个未组装的字节索引和第一个不可接受的字节索引之间的长度。
     size_t window_size() const;
     //!@}
 
@@ -54,6 +57,10 @@ class TCPReceiver {
     size_t unassembled_bytes() const { return _reassembler.unassembled_bytes(); }
 
     //! \brief handle an inbound segment
+    //该函数将会在每次获取到 TCP 报文时被调用
+    //如果接收到了 SYN 包，则设置 ISN 编号。
+    //注意：SYN 和 FIN 包仍然可以携带用户数据并一同传输。同时，同一个数据包下既可以设置 SYN 标志也可以设置 FIN 标志。
+    //将获取到的数据传入流重组器，并在接收到 FIN 包时终止数据传输。
     void segment_received(const TCPSegment &seg);
 
     //! \name "Output" interface for the reader
