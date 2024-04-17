285a9e31f240dda3b86a28a6f9f3b5d884da94a1
coleanthony <971259967@qq.com>
2024-04-17 23:36:46 +0800

    debug tcp_sender

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -3,6 +3,7 @@
 #include "tcp_config.hh"
 
 #include <random>
+#include <optional>
 
 // Dummy implementation of a TCP sender
 
@@ -20,19 +21,102 @@ using namespace std;
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
     : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
     , _initial_retransmission_timeout{retx_timeout}
-    , _stream(capacity) {}
+    , _stream(capacity)
+    ,_timer(retx_timeout) {}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
+//在传输的部分的字节数
+uint64_t TCPSender::bytes_in_flight() const {return _bytes_in_flight;}
 
-void TCPSender::fill_window() {}
+void TCPSender::fill_window() {
+    uint16_t window_size=_window_size?_window_size:1;
+    while(_window_size>_bytes_in_flight){
+        TCPSegment tcpsegment;
+        if(!is_sync_flag){
+            //一开始发SYN包
+            is_sync_flag=true;
+            tcpsegment.header().syn=true;
+        }
+        //目前还能放进的字节数量
+        size_t sendlen=min(TCPConfig::MAX_PAYLOAD_SIZE,(_stream.buffer_size(),window_size-_bytes_in_flight-tcpsegment.header().syn));
+        //读取字节数
+        std::string ss=_stream.read(sendlen);
+        //填充数据
+        tcpsegment.payload()=Buffer(std::move(ss));
 
+        //如果第一次到达FIN
+        if(!is_fin_flag&&tcpsegment.length_in_sequence_space()<window_size&&_stream.eof()){
+            is_fin_flag=true;
+            tcpsegment.header().fin=true;
+        }
+
+        //空包就不发送了
+        if(tcpsegment.length_in_sequence_space()==0){
+            break;
+        }
+
+        tcpsegment.header().seqno=next_seqno();
+        if(!_timer.is_running()){
+            _timer.restart();
+        }
+
+        //发送到_segments_out，保存还在flight的到_segments_in_flight
+        _segments_out.push(tcpsegment);
+        _bytes_in_flight+=tcpsegment.length_in_sequence_space();
+        _segments_in_flight.push(make_pair(_next_seqno,tcpsegment));
+        _next_seqno+=tcpsegment.length_in_sequence_space();
+    }
+}
+
+//1. Set the RTO back to its “initial value.”
+//2. If the sender has any outstanding data, restart the retransmission timer so that it 
+//   will expire after RTO milliseconds (for the current value of RTO).
+//3. Reset the count of “consecutive retransmissions” back to zero.
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
+    //首先将收到的 ackno 转化为 absolute ackno ，便于后续处理已经收到的包
+    uint64_t absolute_ackno=unwrap(ackno,_isn,_next_seqno);
+    // 传入的 ACK 是不可靠的，直接丢弃
+    if(absolute_ackno>_next_seqno)  return;
+
+    while(!_segments_in_flight.empty()){
+        size_t segment_size=_segments_in_flight.front().second.length_in_sequence_space();
+        if(_segments_in_flight.front().first+segment_size<=absolute_ackno){
+            //删除已经收到的包
+            _bytes_in_flight-=segment_size;
+            _segments_in_flight.pop();
+            _timer.reset();
+        }else{
+            break;
+        }
+    }
+
+    //最后将连续重传计数清零。
+    _consecutive_retransmissions_count=0;
+    _window_size=window_size;
+    fill_window();
+}
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+//Periodically, the owner of the TCPSender will call the TCPSender’s tick method, indicating the passage of time.
+void TCPSender::tick(const size_t ms_since_last_tick) {
+    _timer.tick(ms_since_last_tick);
+    //超时且有未发送的包
+    if(_timer.check_time_out()&&!_segments_in_flight.empty()){
+        _segments_out.push(_segments_in_flight.front().second);
+        //重传
+        if(_window_size>0){
+            _timer.set_time_out(_timer.get_time_out()*2);
+            _consecutive_retransmissions_count++;
+        }
+        _timer.restart();
+    }
+}
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+unsigned int TCPSender::consecutive_retransmissions() const { return _consecutive_retransmissions_count; }
 
-void TCPSender::send_empty_segment() {}
+void TCPSender::send_empty_segment() {
+    TCPSegment tcpsegment;
+    tcpsegment.header().seqno=next_seqno();
+    _segments_out.push(tcpsegment);
+}
--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -9,28 +9,69 @@
 #include <functional>
 #include <queue>
 
+//! \brief The timer for TCP's ticker implementation
+class Timer{
+  private:
+    uint32_t _time_count;
+    uint32_t _time_out;
+    bool _is_running;
+  public:
+    Timer()=default;
+    Timer(uint32_t time_out): _time_count(0),_time_out(time_out),_is_running(false){}
+    void start(){_is_running=true;}
+    void stop(){_is_running=false;}
+    void reset(){_time_count=0;}
+    void restart(){_is_running=true,_time_count=0;}
+    bool is_running() {return _is_running;}
+    uint32_t get_time_count() {return _time_count;}
+    uint32_t get_time_out() {return _time_out;}
+    void set_time_out(uint32_t time_out)  {_time_out=time_out;}
+    void tick(size_t ms_since_last_tick)  {
+      if (_is_running){
+        _time_count+=ms_since_last_tick;
+      }
+    }
+    bool check_time_out(){
+      if (_is_running && _time_count>=_time_out){
+        return true;
+      }
+      return false;
+    }
+};
+
 //! \brief The "sender" part of a TCP implementation.
 
 //! Accepts a ByteStream, divides it up into segments and sends the
 //! segments, keeps track of which segments are still in-flight,
 //! maintains the Retransmission Timer, and retransmits in-flight
 //! segments if the retransmission timer expires.
+//1.将 ByteStream 中的数据以 TCP 报文形式持续发送给接收者。
+//2.处理 TCPReceiver 传入的 ackno 和 window size，以追踪接收者当前的接收状态，以及检测丢包情况。
+//3.若经过一个超时时间后仍然没有接收到 TCPReceiver 发送的针对某个数据包的 ack 包，则重传对应的原始数据包。
 class TCPSender {
   private:
     //! our initial sequence number, the number for our SYN.
     WrappingInt32 _isn;
-
     //! outbound queue of segments that the TCPSender wants sent
     std::queue<TCPSegment> _segments_out{};
-
     //! retransmission timer for the connection
     unsigned int _initial_retransmission_timeout;
-
     //! outgoing stream of bytes that have not yet been sent
     ByteStream _stream;
-
     //! the (absolute) sequence number for the next byte to be sent
     uint64_t _next_seqno{0};
+    //! timer
+    Timer _timer;
+    //the number of bytes that have been sent but not acknowledged
+    uint64_t _bytes_in_flight{0};
+    //the initial window size
+    uint64_t _window_size{1};
+    //the number of consecutive retransmission
+    uint64_t _consecutive_retransmissions_count{0};
+    //contain syn flag or fin flay;
+    bool is_sync_flag{false},is_fin_flag{false};
+    //the segments that have been sent but not acknowledged
+    std::queue<std::pair<uint64_t,TCPSegment>> _segments_in_flight{};
 
   public:
     //! Initialize a TCPSender
